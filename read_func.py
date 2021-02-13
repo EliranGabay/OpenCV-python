31c8e8206cd7731482038a123b52c3f9824115d1
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2021-02-13 11:43:51 +0200

    add file basic_func.py

--- read_func.py
+++ read_func.py
@@ -1,12 +1,12 @@
 import cv2
 
 # ____ read image file ____
-img = cv2.imread("")
+img = cv2.imread("")  # path resource
 cv2.imshow("Output", img)
 cv2.waitKey(0)
 
 # ____ read video file ____
-cap = cv2.VideoCapture("")
+cap = cv2.VideoCapture("")  # path resource
 while True:
     success, img = cap.read()
     cv2.imshow("Video", img)
@@ -14,7 +14,7 @@ while True:
         break
 
 # ____ read cam ____
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  # 0 to cam default
 cap.set(3, 640)
 cap.set(4, 480)
 cap.set(10, 100)
