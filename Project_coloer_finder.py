6229503e7b8bddf517ac1689ab1434bfb4161862
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2021-02-13 17:46:52 +0200

    add colorPicker.py

--- Project_coloer_finder.py
+++ Project_coloer_finder.py
@@ -8,6 +8,17 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
 
+myColor = []
+
+
+def findColor(img):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # lower = np.array([h_min, s_min, v_min])
+    # upper = np.array([h_max, s_max, v_max])
+    # mask = cv2.inRange(imgHSV, lower, upper)
+    # cv2.imshow("img", mask)
+
+
 while True:
     success, img = cap.read()
     cv2.imshow("Cam", img)
